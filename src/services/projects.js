5142383f8cc823aea549f7b8a5911dc4281f8f63
george-safwat <=>
2022-05-28 14:21:09 +0200

    react-create-app on Heroku

--- src/services/projects.js
+++ src/services/projects.js
@@ -3,7 +3,7 @@ export default async function projects(query) {
     try {
     
         const limit = query.queryKey[1] || 4
-        const projects = await axios.get("http://localhost:4000/api/v1/projects?limit=" + limit)
+        const projects = await axios.get("https://shielded-woodland-38501.herokuapp.com//api/v1/projects?limit=" + limit)
         return projects.data
     } catch(e) {
         throw e
--- src/services/userInfo.js
+++ src/services/userInfo.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 export default async function userInfo() {
     try {
-        const userData = await axios.get("http://localhost:4000/api/v1/user")
+        const userData = await axios.get("https://shielded-woodland-38501.herokuapp.com//api/v1/user")
         return (userData.data.user)
     } catch(e) {
         throw e
