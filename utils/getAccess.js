5157584a5e5857ab0c7d42cbbaec28ac3451a57c
george-safwat <=>
2022-05-28 16:24:07 +0200

    Added default user

--- utils/getAccess.js
+++ utils/getAccess.js
@@ -8,6 +8,19 @@ exports.protected = catchAsync(async (req, res, next) => {
         
         const user = await UserInfo.findOne()
         
+        if(!user) {
+            const newUser = await UserInfo.insert({
+                email : "georgesafwat@protonmail.com",
+                password : "12345678",
+                phoneNumber : "+201278616724",
+                twitter : "https://twitter.com/georgesafw",
+                instagram : "https://instagram.com/iamgeorgesafwat",
+                title : "CEO & Entrepreneur",
+                available : false,
+                avatar : "https://i.ibb.co/HP7sTWp/photo-2022-05-22-13-18-22.jpg"
+            })
+            return next()
+        }
         if (await user.correctPassword(passwordSent, user.password)) {
             return next()
         } else {
