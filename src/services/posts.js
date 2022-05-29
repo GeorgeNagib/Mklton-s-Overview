4cf5a4d5de5aedb75bcb5117198d5b43bc2ba255
george-safwat <=>
2022-05-29 13:34:54 +0200

    Fixed backend env variable

--- src/services/posts.js
+++ src/services/posts.js
@@ -4,7 +4,7 @@ export default async function(query) {
         const limit = query.queryKey[1] || 4
         const searchQuery = query.queryKey[2].trim() || ""
         let posts;
-        
+        console.log(process.env.BACKEND_URL)
         if(searchQuery.trim().length > 0) {
             posts = await axios.get(process.env.BACKEND_URL + "/api/v1/posts/search/" + searchQuery + "?limit=" + limit)
             return {isSearch: true, posts: {sentPosts: posts.data.data}, allPostsLength: posts.data.allPostsLength}
