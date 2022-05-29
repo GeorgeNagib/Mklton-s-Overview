20cd0a7551396514829245566de0f464d7d238b7
george-safwat <=>
2022-05-29 13:39:59 +0200

    added backend url maually

--- src/services/posts.js
+++ src/services/posts.js
@@ -4,12 +4,12 @@ export default async function(query) {
         const limit = query.queryKey[1] || 4
         const searchQuery = query.queryKey[2].trim() || ""
         let posts;
-        console.log(process.env.BACKEND_URL)
+
         if(searchQuery.trim().length > 0) {
-            posts = await axios.get(process.env.BACKEND_URL + "/api/v1/posts/search/" + searchQuery + "?limit=" + limit)
+            posts = await axios.get("https://mklton-s-overview-backend-dik1khtf1-george-safwat.vercel.app/api/v1/posts/search/" + searchQuery + "?limit=" + limit)
             return {isSearch: true, posts: {sentPosts: posts.data.data}, allPostsLength: posts.data.allPostsLength}
         } else {
-            posts = await axios.get(process.env.BACKEND_URL + "/api/v1/posts?limit=" + limit)
+            posts = await axios.get("https://mklton-s-overview-backend-dik1khtf1-george-safwat.vercel.app/api/v1/posts?limit=" + limit)
         }
 
 
