9e70ea8f19822b1699e801325097f8e8a0a4f005
george-safwat <=>
2022-05-29 13:52:26 +0200

    Added Blog's url

--- models/postsModel.js
+++ models/postsModel.js
@@ -11,6 +11,11 @@ const postSchema = new mongoose.Schema({
         trim: true,
         required: [true, "Tell us what do you want to say."]
     },
+    url: {
+        type: String,
+        trim: true,
+        required: [true, "Send us blog's URL"]
+    },
     createdAt: {
         type: Date,
         default: Date.now()
